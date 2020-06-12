0902cfd6a54360ce6e02266e14c3d4a19ed7fd9a
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-06-12 10:45:19 -0700

    1st submit for Sprint Challenge

--- index.js
+++ index.js
@@ -208,10 +208,12 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+// console.log(artists[0].name);
+// console.log(artists[2].bio);
 
-
-/* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
+// /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+// artists[8].name = 'Vincent Van Gogh';
+// console.log(artists[8].name)
 
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
@@ -222,21 +224,25 @@ const artists = [
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(array, index) {
-    /* code here */
-  }
-  
+// function getArtistByIndex(array, index) {
+//    return `The artist at index ${array[index].id} is ${array[index].name}.`
+//   }
+  // console.log(getArtistByIndex(artists,0))
+
   /**
 
 
-/* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who lived in the 20th century (1900-2000) */
+/* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who lived in the 20th century (1900-2000)
 
-function get20s(/* Code here */){
+1- data : as an argument 
+2- return an arr: names of artists who lived in 20 century.
 
-  /* Code here */
+*/
 
-}
 
+// function get20s{
+
+// }
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
  *     (1) artists array
@@ -248,11 +254,11 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(arr, index) {
+    arr.splice(index, 1)
   }
-  
-  /**
+console.log(removeArtist(artists,0)); 
+ 
 
 
 
